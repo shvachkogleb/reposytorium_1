376c0f69ab113aa3d4c9bfd94a3c312a9d8116d8
HLib Shvachko <Shvachko.Hlib@SZKOLA.ZST-RADOM.EDU.PL>
2024-09-27 16:45:39 +0200

    apl1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            List<string> firstlist = new List<string> { };
+            while (a) {
+                string m = Console.ReadLine();
+                if (m == "-")
+                {
+                    a = false;
+                }
+                else {
+                    firstlist.Add(m);
+                }
+            }
+            firstlist.Sort();
+            foreach (string name in firstlist)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/substr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class substr
+    {
+        public string Cutd(List<string> firstlist, bool a)
+        { 
+            firstlist.Sort();
+            foreach (string name in firstlist)
+                {
+                    return name;
+                }
+            }
+        }
+        }
+    }
+}
